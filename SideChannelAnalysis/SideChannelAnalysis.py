f606777917304d1e6f43c8d027614f40051f77b8
yacomelian <yacomelian@gmail.com>
2018-10-21 10:06:48 +0100

    Añadiendo comprobación de claves

--- SideChannelAnalysis/SideChannelAnalysis.py
+++ SideChannelAnalysis/SideChannelAnalysis.py
@@ -13,10 +13,10 @@ pip install path.py numpy scipy matplotlib
 """
 
 import os, sys, datetime, path
-import numpy, scipy.stats, matplotlib.pyplot as plt
-import binascii, Crypto
+import random, numpy, scipy.stats, matplotlib.pyplot as plt
+import binascii
 
-#from Crypto.Cipher import AES
+
 #from binascii import unhexlify
 
 __author__ = "Yaco Melian"
@@ -28,17 +28,13 @@ __email__ = "yacomelian@gmail.com"
 __status__ = "Production"
 
 ### Variables de trabajo
-workpath = os.getcwd() #"C:/Users/zzz/OneDrive/Documentos/00 - UOC/09 Seminarios en empresa (casos B) aula 1/PEC/"
-#workpath1 = os.path.realpath(__file__)
+workpath = os.getcwd() + "/"
 mostrargrafica = True
 cargarficheros = True
 sobreescribirficheros = False
  
-
-
 ### Constantes del programa
 # Tabla de búsqueda de operación SubBytes
-#sBox    = [ 0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76, 0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0, 0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15, 0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75, 0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84, 0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf, 0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8, 0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2, 0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73, 0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb, 0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79, 0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08, 0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a, 0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e, 0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf, 0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 ]
 sBox = (
             0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
             0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -72,13 +68,13 @@ def hamming_weight( x ):
 
 # Resuelve preguntas de si, no    
 def query_yes_no(question, default="no"):
-
     """Realiza na pregunta de si o no via input() y devuelve la respuesta.
     "question" es una cadena que se mostrara al usuario.
     "default" es la respuesta predeterminada
     La respuesta se devuelve como True para si, False para no.
     """
     valid = {"si": True, "s": True, 
+             "yes":True, "y" :True,
              "no": False, "n": False}
     if default is None:
         prompt = " [s/n] "
@@ -106,9 +102,10 @@ def query_yes_no(question, default="no"):
 # Funcion principal de calculo
 def sca (ldatos, ltraza):
     # Para operaciones posteriores, calculo tamaños de los ficheros
-    numberofmessages = len (ldatos)
-    numberoftraces = len(ltraza)
-    lenoftraces = int(len(ltraza[0])/2)
+    numberofmessages = len (ldatos)         # Número de mensajes
+    numberoftraces = len(ltraza)            # Longitud total de la línea de trazas
+    lenoftraces = int(len(ltraza[0])/2)     # Número total de trazas
+    numberofkeys = 256
 
     #Convierto fichero de trazas de texto a enteros
     # Reservo una matriz primero, optimizacion
@@ -118,28 +115,24 @@ def sca (ldatos, ltraza):
     
     # Guardamos la matrix transpuesta para calcular el coeficiente.
     ti_trans = numpy.matrix.transpose(ti)
-    key = [ 0 ] * 16
-    instant = [ 0 ] * 16
-    qpi = [0] * 16
+    key = [ 0 ] * 16            # Reservo un vector para almacenar la clave
+    instant = [ 0 ] * 16        # Reservo un vector para almacenar el instante de mayor correlación
+    qpi = [0] * 16              # Vector para almacenar todas las matrices de correlación de cada valor de clave
     
-    for bytemensaje in range (0,32,2):
-        
-        # Para cada valor posible de la clave
-        # Almaceno los coeficientes, para cada byte de la posible clave, tendre 256 vectores(arrays), cada vector con 480 componentes, y esos componentes serán el valor de los coeficientes de pearson
+    for bytemensaje in range (0,32,2):  # Para cada byte de entrada del mensaje
         cnt=int((bytemensaje/2)+1)  # Cuento el byte que estoy procesando
-        print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M ") + "Procesando byte " + str(cnt) + " de 16. ")
+        print (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S ") + "Procesando byte " + str(cnt) + " de 16. ")
 
         # Este if es para controlar cuando leo los resultados de los ficheros, o vuelvo a recalcular todo.
         filename = workpath + "result_" + str(cnt) + ".matrix.npy"
         filecache = path.Path(filename).isfile();               # Compruebo si el fichero con resultados guardados existe
         if (not (cargarficheros and filecache)):
-            pi = numpy.zeros((256,lenoftraces)) # Matriz donde almaceno los coeficientes de pearson (256 posibles claves x el numero de trazas
-            for valorclave in range(0x00,0xFF): # Para cada posible clave
-                # Para cada mensaje
+            pi = numpy.zeros((numberofkeys,lenoftraces)) # Matriz donde almaceno los coeficientes de pearson (256 posibles claves x el numero de trazas
+            for valorclave in range(0x00,0x100): # Para cada posible clave
                 xi = [ valorclave ] * numberofmessages  # Inicializo vector para almacenar 
                 si = [ 0 ] * numberofmessages           # Inicializo vector SubBytes
                 hi = [ 0 ] * numberofmessages           # Inicializo vector pesos de Hamming
-                
+
                 for mi in range (0,len(ldatos)):
                     # Realizo la funcion XOR del valor clave (ya contenido en xi) con el byte correspondiente del mensaje
                     xi[mi] ^=  int(ldatos[mi][bytemensaje:bytemensaje+2],16)
@@ -149,7 +142,7 @@ def sca (ldatos, ltraza):
                     hi[mi] = hamming_weight (si[mi])
                     # Con esto, ya tengo los datos de entrada, para calcular pearson, tengo que relacionar los pesos de hamming frente a las trazas
                     # Solo para debug, mostrar:
-                    #    print ("XOR: ",valorclave," ^ ", ldatos[mi][bytemensaje:bytemensaje+2], " = ", xi[mi], " SubBytes: ", si[mi], " Peso Hamming: ", hi[mi])
+                    #print ("XOR: ",valorclave," ^ ", ldatos[mi][bytemensaje:bytemensaje+2], " = ", xi[mi], " SubBytes: ", si[mi], " Peso Hamming: ", hi[mi])
                 
                 # Calculo el coeficiente de pearson entre los pesos de haming y cada una de las trazas
                 for t in range (0, lenoftraces):
@@ -164,64 +157,73 @@ def sca (ldatos, ltraza):
             pi = numpy.load(filename)
             
         # Busco el valor de mayor correlación
-        max = numpy.max(pi)         # Valor de correlación máximo en momento (tiempo)
-        min = numpy.min(pi)         # Valor de correlación negativa máxima
-        mmax = numpy.argmax(pi)     # Posición en el máximo
-        mmin = numpy.argmin(pi)     # Posición en el mínimo
-        if (abs(max) > abs(min)):   # Aquí valoro el momento de mayor correlación
-            momento = mmax % 480    
-        else:
-            momento = mmin % 480
+        momento = numpy.argmax(pi) % lenoftraces     # Posición (instante) de correlación máxima
         instant[cnt-1] = momento        # Vector con instantes
         
         # Transpongo la matriz porque la funcion busca los máximos por fila, así facilito el cálculo
         pi_trans = numpy.matrix.transpose(pi)
-        # La clave estará sera el punto de mayor correlacion, modulo 256, porque son el numero de posibles claves
-        max = numpy.max(pi_trans)       # Valor de correlación máximo en clave
-        min = numpy.min(pi_trans)       # Valor de correlación negativa máxima de clave 
-        cmax = numpy.argmax(pi_trans)   # Clave del máximo
-        cmin = numpy.argmin(pi_trans)   # Clave del mínimo
-        if (abs(max) > abs(min)):       # Punto de mayor correlación (será la clave)
-            clave = cmax % 256
-        else:
-            clave = cmin % 256
-        key[cnt-1] = clave%256          # Vector con claves
-        qpi[cnt-1] = pi
+        # La clave sera el punto de mayor correlacion, modulo 256, porque son el numero de posibles claves
+        cmax = numpy.argmax(pi_trans)%numberofkeys   # Clave del máximo
+        key[cnt-1] = cmax               # Vector con claves
+        qpi[cnt-1] = pi                 # Vector con correlaciones, para gráficas
+
         # El momento nos indica un valor de tiempo, de cuando fue procesada esa clave, debe ocurrir, que cada byte de
         # clave se haya calculado en su momento, es decir, el primer byte de clave, es el primero que aparece en las trazas
         print ("Maximo encontrado en el momento  :", momento, "Clave : ", key[cnt-1] )
 
-    sca = (
-        key,
-        instant,
-        qpi
-    )
+    return ( key, qpi)
 
-    return sca
+def comprobar_clave (ldatos, key):
+    import Crypto.Cipher
+    from Crypto.Cipher import AES
+    numberoftests = 5                   # Al azar elijo el número de pruebas a realizar
+    for i in range (0,numberoftests):      # Hago múltiples comprobaciones
+        mensaje = random.randint(0,len(ldatos))   # Elijo mensaje aleatorio de los 5mill
+        textoacifrar = ldatos[mensaje][:32]
+        cifradocorrecto = ldatos[mensaje][32:]
+        textocifrado = cifradocorrecto
+        #cipher = unhexlify('9B2808B4743D822697AD55243F956A39')
+        textocifrado = AES.new(key, AES.MODE_CBC, iv)
+        print ("Texto a cifrar: ", textoacifrar)
+        print ("Cifrado correcto:", cifradocorrecto)
+        if (textoacifrar == cifradocorrecto):
+            print ("La clave es correcta")
+        else:
+            print ("Error: la clave es incorrecta")
+
+
+    #Clave CAFEBABEDEADBEEFBADAB000FFAAAAAA
+    
+
+    #cipher = unhexlify('9B2808B4743D822697AD55243F956A39')
+    #aes = AES.new(key, AES.MODE_CBC, iv)
+ 
+    #print ( aes.decrypt(cipher.decode('hex')))
+    
 
 def main ():
     
     # Defino los ficheros de entrada
-    str_fdatos= workpath + "/datos.txt" # contiene en cada fila un mensaje de entrada del algoritmo y, a continuación, el texto cifrado de salida (formato hexadecimal).
-    str_ftraza= workpath + "/trazas.txt" # contiene en cada fila la señal de consumo de la ejecución del algoritmo en un dispositivo hardware
+    str_fdatos= workpath + "datos.txt" # contiene en cada fila un mensaje de entrada del algoritmo y, a continuación, el texto cifrado de salida (formato hexadecimal).
+    str_ftraza= workpath + "trazas.txt" # contiene en cada fila la señal de consumo de la ejecución del algoritmo en un dispositivo hardware
 
     if (path.Path(str_fdatos).isfile() and  path.Path(str_ftraza).isfile()):
     # Toma de datos para ejecución
         global cargarficheros, sobreescribirficheros, mostrargrafica
-        cargarficheros = query_yes_no("¿Usar ficheros?")
+        cargarficheros = query_yes_no("¿Usar ficheros?", "si")
         if (not cargarficheros):
             sobreescribirficheros = query_yes_no("¿Desea sobreescribir los ficheros (No para mantener los actuales)?")
         mostrargrafica = query_yes_no("¿Desea visualizar graficas?")
+        comprobarclave = query_yes_no("¿Desea comprobar la clave?","si")
 
         # Leo ambos ficheros y los almaceno en memoria
         fdatos=open(str_fdatos,"r")
         ftraza=open(str_ftraza,"r")
-
         ldatos = fdatos.readlines()
         ltraza = ftraza.readlines()
     
         # Inicio el análisis
-        result = sca(ldatos,ltraza)
+        result = sca(ldatos,ltraza)         # Devuelve un array con 2 objetos 0: clave, 1:Matrices para las gráficas
 
          # Muestro la clave obtenida
         print ("")
@@ -232,9 +234,9 @@ def main ():
         # Dibujar las gráficas
         if (mostrargrafica == True):
             print ("Dibujando gráficas...")
-            for i in range (len(result[2])):
+            for i in range (len(result[1])):
                 plt.figure("Evaluación de clave para Byte " + str(i+1))
-                plt.plot(result[2][i])
+                plt.plot(result[1][i])
                 plt.xlabel("Evaluación de clave")
                 plt.ylabel("Coeficiente de Pearson")
                 plt.title ("Correlación de claves " + str(i+1))
@@ -242,13 +244,8 @@ def main ():
             plt.show()
             plt.close
 
-    #CAFEBABEDEADBEEFBADAB0000AAAAAAA
-    #CAFEBABEDEADBEEFBADAB0000AAAAAAA
-
-    #cipher = unhexlify('9B2808B4743D822697AD55243F956A39')
-    #aes = AES.new(key, AES.MODE_CBC, iv)
- 
-    #print ( aes.decrypt(cipher.decode('hex')))
+        if (comprobarclave == True):
+            comprobar_clave (ldatos, result[0])
     else:
         print ("Error: No existen los archivos de entrada de datos en la ruta:")
         print (workpath)
@@ -259,13 +256,3 @@ if sys.version_info[0] != 3:
     sys.exit(1)
     
 main()
-
-'''
-De las 256 curvas de correlación obtenidas, una por cada hipótesis del valor del primer byte de la clave, ordenarlas en función de la amplitud del mayor pico de correlación observado. 
-Con esto se establecerá un ránking de probabilidad del valor de la clave.
-'''
-
-
-
-#5. Si se ha hecho correctamente, el valor del primer byte de la clave corresponde Si se ha hecho correctamente, el valor del primer byte de la clave corresponde a la curva con el pico de mayor amplitud.
-#6. Repetir los pasos anteriores para los restantes 15 bytes.
